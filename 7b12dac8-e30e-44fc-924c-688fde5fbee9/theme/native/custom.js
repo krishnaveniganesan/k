af40803673690a8d699fc32dbd83c2387d7d6135
Ubuntu <ubuntu@ip-172-31-7-226.us-west-1.compute.internal>
2022-09-21 11:12:43 +0000

    first commit

---
+++ 7b12dac8-e30e-44fc-924c-688fde5fbee9/theme/native/custom.js
@@ -0,0 +1,680 @@
+export const frontPageContent1 = {
+    container: {
+        backgroundColor: '#E6ECFA',
+        borderRadius: 2,
+        height: 20,
+    },
+    lable: {
+        Color: '#323C60',
+        fontSize: 40,
+        lineHeight: 15
+    }
+}
+export const frontPageContent2 = {
+    container: {
+        backgroundColor: '#FAE2E2',
+        borderRadius: 2,
+        height: 20,
+
+
+    },
+    lable: {
+        Color: '#F5523C',
+        fontSize: 12,
+        lineHeight: 15
+    }
+}
+export const frontPageContent3 = {
+    container: {
+        backgroundColor: '#F7E4FF',
+        borderRadius: 2,
+        height: 20,
+
+    },
+    lable: {
+        Color: '#7D437D',
+        fontSize: 12,
+        lineHeight: 15
+    }
+}
+export const frontPageContent4 = {
+    container: {
+        backgroundColor: '#F7ECD0',
+        borderRadius: 2,
+        height: 20,
+    },
+    lable: {
+        Color: '#BB9C59',
+        fontSize: 12,
+        lineHeight: 15
+    }
+}
+export const frontPageContent5 = {
+    container: {
+        backgroundColor: '#def2c1',
+        borderRadius: 2,
+        height: 20,
+    },
+    lable: {
+        Color: '#BB9C59',
+        fontSize: 12,
+        lineHeight: 15
+    }
+}
+export const frontPageContent6 = {
+    container: {
+        backgroundColor: '#e7e8ec',
+        borderRadius: 2,
+        height: 20,
+    },
+    lable: {
+        Color: '#BB9C59',
+        fontSize: 12,
+        lineHeight: 15
+    }
+}
+export const grayText = {
+    lable: {
+        Color: '#333D66'
+    }
+
+}
+export const BorderBottomLine1 = {
+    container: {
+        borderBottomWidth: 3,
+        borderColor: '#ECEDF7',
+        width: '100%',
+    },
+
+}
+export const whiteBorder = {
+    container: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        borderColor: '#ECEDF7',
+        borderWidth: 1,
+        height: 50
+    }
+}
+export const bgColor = {
+    container: {
+        backgroundColor: "#ffff"
+    }
+}
+export const whiteText = {
+    lable: {
+        Color: '#fff'
+    }
+}
+export const hide = {
+    condition: {
+        opacity: 0,
+        height: 0,
+        width: 0
+    },
+    text: {
+        opacity: 0,
+        height: 0,
+        width: 0
+    }
+}
+export const fontsize8 = {
+    text: {
+        fontSize: 12,
+        lineHeight: 11
+    },
+    input: {
+        fontSize: 12,
+        lineHeight: 11
+    }
+}
+export const font10 = {
+    text: {
+        fontSize: 10,
+        lineHeight: 14
+    },
+    input: {
+        fontSize: 10,
+        lineHeight: 14
+    }
+}
+export const font11 = {
+    text: {
+        fontSize: 11,
+        lineHeight: 15
+    },
+    input: {
+        fontSize: 11,
+        lineHeight: 14
+    }
+}
+export const font12 = {
+    text: {
+        fontSize: 12,
+        lineHeight: 15
+    },
+    input: {
+        fontSize: 12,
+        lineHeight: 15
+    }
+}
+export const font13 = {
+    text: {
+        fontSize: 13,
+        lineHeight: 17
+    },
+    input: {
+        fontSize: 13,
+        lineHeight: 17
+    }
+}
+export const font14 = {
+    text: {
+        fontSize: 14,
+        lineHeight: Platform.OS === 'ios' ? 20 : 20,
+    },
+    input: {
+        fontSize: 14,
+        lineHeight: 18
+    }
+}
+export const font15 = {
+    text: {
+        fontSize: 15,
+        lineHeight: 20
+    },
+    input: {
+        fontSize: 15,
+        lineHeight: 20
+    }
+}
+export const font16 = {
+    text: {
+        fontSize: 16,
+        lineHeight: 22
+    },
+    input: {
+        fontSize: 16,
+        lineHeight: 20
+    }
+}
+export const font18 = {
+    text: {
+        fontSize: Platform.OS === 'ios' ? 18 : 18,
+        lineHeight: Platform.OS === 'ios' ? 25 : 25,
+    },
+    input: {
+        fontSize: 18,
+        lineHeight: 20
+    }
+}
+export const font20 = {
+    text: {
+        fontSize: 20,
+        lineHeight: 27
+    },
+    input: {
+        fontSize: 20,
+        lineHeight: 26
+    }
+}
+export const font20a = {
+    text: {
+        fontSize: 20,
+        lineHeight: 30
+    },
+    input: {
+        fontSize: 20,
+        lineHeight: 26
+    }
+}
+export const font21 = {
+    text: {
+        fontSize: 21,
+        lineHeight: 27
+    },
+    input: {
+        fontSize: 21,
+        lineHeight: 25
+    }
+}
+export const font30 = {
+    text: {
+        fontSize: 30,
+        lineHeight: 38
+    },
+    input: {
+        fontSize: 30,
+        lineHeight: 35
+    }
+}
+
+/* Font Weights */
+export const textBold = {
+    text: {
+        fontWeight: "bold"
+    }
+}
+
+/* Colors */
+export const greyText = {
+    text: {
+        color: "#333333",
+    }
+}
+export const greyText1 = {
+    text: {
+        color: "#9EA4B3",
+    }
+}
+
+export const whiteText1 = {
+    text: {
+        color: "#fff",
+    }
+}
+export const Text1 = {
+    text: {
+        color: "#323C60",
+    }
+}
+export const Text2 = {
+    text: {
+        color: "#F5523C",
+    }
+}
+export const Text3 = {
+    text: {
+        color: "#7D437D",
+    }
+}
+export const Text4 = {
+    text: {
+        color: "#BB9C59",
+    }
+}
+export const Text5 = {
+    text: {
+        color: "#333D66",
+    }
+}
+export const Text6 = {
+    text: {
+        color: "#BB9C59",
+    }
+}
+export const colorBorderCon = {
+    container: {
+        width: 25,
+        height: 140,
+        backgroundColor: "#c1d4f4",
+        position: "relative",
+        margin: 0,
+        padding: 0,
+    }
+}
+export const imageCon = {
+    container: {
+        position: "absolute",
+    }
+}
+export const pushnoimg = {
+    container: {
+        borderColor: "white",
+        borderWidth: 2,
+        borderRadius: 25,
+
+    }
+}
+export const noMargin = {
+    container: {
+        margin: 0,
+        padding: 0
+    }
+}
+export const colorBorderCon1 = {
+    container: {
+        width: 25,
+        height: 100,
+        backgroundColor: "#d4c0f7",
+        position: "relative",
+        margin: 0,
+        padding: 0
+    }
+}
+
+export const roboto300 = {
+    text: {
+        fontFamily: "Roboto",
+        fontWeight: '300',
+        fontStyle: 'normal',
+    }
+}
+
+export const roboto400 = {
+    text: {
+        fontFamily: "Roboto",
+        fontWeight: '400',
+        fontStyle: 'normal',
+    }
+}
+
+export const roboto500 = {
+    text: {
+        fontFamily: "Roboto",
+        fontWeight: '500',
+        fontStyle: 'normal',
+    }
+}
+
+export const roboto600 = {
+    text: {
+        fontFamily: "Roboto",
+        fontWeight: '600',
+    }
+}
+
+export const roboto700 = {
+    text: {
+        fontFamily: "Roboto",
+        fontWeight: '700',
+    }
+}
+export const ReferenceSelector = {
+    container: {
+        borderWidth: 0,
+    }
+}
+export const TabContainer = {
+        tabBar: {
+            shadowColor: "#fff",
+            shadowOffset: {
+                width: 0,
+                height: 0,
+            },
+            shadowOpacity: 0,
+            shadowRadius: 0,
+            elevation: 0,
+        },
+
+        tab: {
+            borderRightWidth: 1,
+            //borderLeftWidth:2,
+            borderColor: '#e2e2e2'
+        },
+        indicator: {
+            //backgroundColor:'#000',
+            borderRightWidth: 1,
+            borderColor: '#e2e2e2'
+        }
+    }
+    //        borderRightWidth:2,
+    //borderColor:'#000'
+
+export const navigationStyle = {
+    bottomBar: {
+        container: {
+            paddingTop: 15,
+            backgroundColor: "#0044BD",
+            height: 65,
+            // borderTopWidth: .5,
+            //borderTopColor: "#e6e6e6",
+            marginBottom: -2,
+            alignItems: "center",
+            // borderWidth:10,
+            // borderColor:"#fff",
+            //borderRadius:5
+        },
+        icon: {
+            color: "#000",
+            alignItems: "center",
+            // padding:2
+        },
+        selectedIcon: {
+            color: "#ae1e37",
+            // paddingHorizontal:footerPad,
+            borderRadius: 22,
+            backgroundColor: "#0e58dc",
+            // borderTopWidth:1,
+            // borderTopColor: "#b71e3d"
+            // borderBottomWidth:1,
+            // borderColor: "#b71e3d",
+            // borderBottomColor: "#b71e3d"
+            paddingTop: 8,
+            width: 45,
+            height: 45,
+            alignItems: "center",
+            borderWidth: 2,
+            borderColor: "#fff",
+            paddingLeft: 1,
+        },
+        selectedLabel: {
+            color: "#ae1e37",
+            // backgroundColor:"#efdfe3"
+        },
+        label: {
+            color: "#000",
+            position: "relative",
+            // top:-3,
+            marginHorizontal: "auto",
+            textAlign: "center",
+            fontFamily: "Dubai",
+            fontSize: 12,
+            // fontSize:fontscale>=1 ? (1/fontscale)*11 : 12,
+            lineHeight: 16
+        }
+    }
+}
+
+export const spacingLeft = {
+    container: {
+        marginLeft: -50,
+    },
+}
+export const bgBorder = {
+    container: {
+        borderRadius: 5,
+        backgroundColor: "#0E58DC",
+        height: 90,
+        width: 90,
+        alignItems: "center",
+        justifyContent: "center"
+
+
+    },
+}
+export const bgBorder1 = {
+    container: {
+        borderRadius: 20,
+        backgroundColor: "#8091A5",
+        height: 40,
+        width: 40,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+}
+export const bgBorder2 = {
+    container: {
+        borderRadius: 20,
+        backgroundColor: "#F5523C",
+        height: 40,
+        width: 40,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+}
+export const bgBorder3 = {
+    container: {
+        borderRadius: 20,
+        backgroundColor: "#800080",
+        height: 40,
+        width: 40,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+}
+export const bgBorder4 = {
+    container: {
+        borderRadius: 20,
+        backgroundColor: "#32C284",
+        height: 40,
+        width: 40,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+}
+export const bgBorder5 = {
+    container: {
+        borderRadius: 20,
+        backgroundColor: "#175DFB",
+        height: 40,
+        width: 40,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+}
+export const spacingLeft1 = {
+    container: {
+        marginLeft: -40,
+    },
+}
+export const spacingLeft2 = {
+    container: {
+        marginLeft: -30,
+    },
+}
+export const bgBorder6 = {
+    container: {
+        borderRadius: 7,
+        backgroundColor: "#0044BD",
+        height: 40,
+        width: 40,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+}
+export const BorderBottomLine2 = {
+    container: {
+        borderBottomWidth: 1,
+        borderColor: '#C5CBDC',
+    },
+
+}
+export const fontNo121 = {
+    text: {
+        color: "#FFFFFF",
+
+        opacity: 0.5,
+    },
+}
+
+export const marginTopNeg = {
+    container: {
+        marginTop: -30,
+    },
+}
+export const marginTopNeg2 = {
+    container: {
+        marginTop: -50,
+    },
+}
+export const marginTopNeg1 = {
+    container: {
+        marginTop: -15,
+    },
+}
+export const spacingLeft3 = {
+    container: {
+        marginLeft: 80,
+    },
+}
+export const spacingLeft7 = {
+    container: {
+        marginLeft: -150,
+    },
+}
+export const spacingLeft4 = {
+    container: {
+        marginLeft: 65,
+    },
+}
+export const borderGeneral = {
+    container: {
+        backgroundColor: "#E8ECF4",
+        height: 30
+    },
+}
+export const noBorderTextbox = {
+    input: {
+        borderWidth: 0,
+    },
+}
+export const fieldsColor = {
+    container: {
+        Color: "#333D66",
+        height: 30
+    },
+}
+export const signature = {
+    container: {
+        height: 500,
+    }
+}
+export const noBorderLine = {
+    container: {
+        borderWidth: 0,
+        backgroundColor: '#fff',
+        borderColor: 'none'
+    }
+}
+export const TextBox1 = {
+    label: {
+        color: "#000",
+    },
+    input: {
+        backgroundColor: "#fff",
+        borderWidth: 0,
+    },
+    inputDisabled: {
+        color: "#000",
+        backgroundColor: "#fff"
+    },
+}
+export const BorderBottomLineThin = {
+    container: {
+        borderBottomWidth: 1,
+        borderColor: '#CCC',
+        width: '95%',
+        marginLeft: "auto",
+        marginRight: "auto",
+        zIndex: 1,
+        position: "relative"
+    },
+
+}
+export const newCard = {
+    container: {
+        backgroundColor: '#FFFFFF',
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop: 5,
+        marginHorizontal: 5,
+        // overflow:"hidden",
+        marginBottom: 20,
+        zIndex: 99999,
+        width: "96%",
+        height: "auto",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 0,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 6,
+    }
+}
---
+++ 7b12dac8-e30e-44fc-924c-688fde5fbee9/theme/native/native.js
@@ -0,0 +1,703 @@
+export const bg = {
+    container: {
+        backgroundColor: "#0044BD"
+    }
+}
+export const bg1 = {
+    container: {
+        backgroundColor: "#0044BD",
+        height: "20%",
+        width: "100%"
+    }
+}
+
+
+export const frontPageContent = {
+    container: {
+
+        borderTopLeftRadius: 24,
+        borderTopRightRadius: 24,
+        height: 650,
+        paddingTop: 30,
+        marginTop: 10,
+        backgroundColor: "#fff",
+
+    }
+}
+
+export const profileImage = {
+    container: {
+        paddingTop: 20,
+    },
+
+}
+
+
+export const activeIcon = {
+    container: {
+        marginBottom: 60,
+        marginTop: -20,
+    }
+}
+export const notifyBigNumberFont1 = {
+    container: {
+        backgroundColor: '#0E58DC',
+        borderRadius: 30,
+        borderWidth: 2,
+        borderColor: "#ffffff",
+        width: 50,
+        height: 50,
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+
+        color: "#ffffff",
+        textAlign: "center",
+        fontWeight: "bold",
+        alignSelf: "center",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    image: {
+        position: "absolute",
+    }
+}
+
+export const flexBox1 = {
+    container: {
+        backgroundColor: '#0039e6',
+        height: 90,
+        width: 90,
+        borderRadius: 4,
+    }
+}
+
+export const flexBox2 = {
+    container: {
+        backgroundColor: '#8E2DE2',
+        borderRadius: 5,
+        height: 90,
+        width: 90,
+        alignItems: "center",
+        justifyContent: "center",
+    }
+}
+
+export const flexBox3 = {
+    container: {
+        backgroundColor: '#7303C0',
+        borderRadius: 5,
+        height: 90,
+        width: 90,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+}
+
+export const flexBox4 = {
+    container: {
+        backgroundColor: '#ECEDF7',
+        height: 70,
+        width: 100,
+        borderRadius: 4,
+    }
+}
+
+export const flexIcon = {
+    container: {
+        paddingTop: 80,
+    },
+    image: {
+        position: "absolute",
+        height: 20,
+        width: 20,
+        color: "white"
+    }
+}
+
+
+export const layoutGrid5 = {
+    container: {
+        marginTop: 15,
+    },
+}
+
+
+export const layoutGrid6 = {
+    text: {
+        color: "#175DFB",
+    }
+}
+
+export const landingPage = {
+    container: {
+        paddingTop: 300,
+    }
+}
+
+export const card1 = {
+    container: {
+        backgroundColor: "#FFFFFF",
+        marginTop: 5,
+        marginBottom: 3,
+        shadowColor: "white",
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        height: 140,
+    },
+};
+
+export const fontPageRequestImage = {
+    container: {
+        backgroundColor: "#0A53D4",
+        opacity: 0.25,
+    }
+}
+
+export const height1 = {
+    container: {
+        height: 175,
+    }
+}
+
+export const height2 = {
+    container: {
+        height: 30,
+        width: 76,
+    },
+    text: {
+        color: "#171E3A",
+    }
+}
+
+export const font1 = {
+    text: {
+        fontFamily: "Roboto",
+        fontWeight: '400',
+        fontStyle: 'italic',
+        fontSize: 12,
+        lineHeight: 14,
+        color: "#9EA4B3",
+    }
+}
+export const font2 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        lineHeight: 28,
+        fontWeight: '500',
+        fontSize: 18,
+        color: "#333D66",
+    },
+}
+
+
+export const font4 = {
+    text: {
+        fontFamily: "Roboto",
+        fontWeight: '400',
+        fontSize: 10,
+        color: "#333D66",
+        lineHeight: 12,
+        fontStyle: 'normal',
+    }
+}
+
+export const font5 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        lineHeight: 14,
+        fontWeight: '400',
+        fontSize: 14,
+        color: "#171E3A",
+    },
+}
+
+export const font6 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: 12,
+        lineHeight: 18,
+        color: "#9EA4B3",
+    }
+}
+
+export const font7 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 15,
+        lineHeight: 28,
+        color: "#FAFBFC",
+    }
+}
+
+export const font8 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 14,
+        lineHeight: 20,
+        color: "#333D66",
+    }
+}
+
+export const fontNo9 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: 14,
+        lineHeight: 14,
+        color: "#333D66",
+    }
+}
+
+export const fontNo10 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: 12,
+        lineHeight: 14,
+        color: "#333D66",
+    }
+}
+
+
+export const fontNo11 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: 12,
+        lineHeight: 14,
+        color: "#175DFB",
+        opacity: 0.6,
+    }
+}
+
+export const fontNo12 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 12,
+        lineHeight: 28,
+        color: "#FFFFFF",
+        opacity: 0.5,
+    },
+}
+
+export const fontNo13 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 12,
+        lineHeight: 28,
+        color: "#FFFFFF",
+    },
+}
+
+export const fontNo14 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 14,
+        lineHeight: 28,
+        color: "#333D66",
+    },
+}
+
+export const fontNo15 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 12,
+        lineHeight: 16,
+        color: "#FAFBFC",
+    },
+}
+
+//------------------------------ CommentPage1 start------------------------------//
+
+export const fontNo16 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: 16,
+        lineHeight: 32,
+        color: "#333D66",
+        opacity: 0.8,
+    },
+}
+
+export const commentTextArea = {
+    container: {
+        boxSizing: "border-box",
+        width: '100%',
+        height: 181,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 8,
+        borderColor: '#8091A5',
+        borderWidth: 2,
+
+    },
+    input: {
+        flex: 'none',
+        alignSelf: 'stretch',
+        flexGrow: 0,
+    }
+}
+
+export const fontNo17 = {
+    container: {
+        width: 214,
+        height: 24,
+    },
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: 12,
+        lineHeight: 32,
+        color: "#171E3A",
+        opacity: 0.5,
+        display: 'flex',
+    },
+}
+export const fontNo18 = {
+    container: {
+        width: 37,
+        height: 24,
+    },
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 12,
+        lineHeight: 24,
+        color: "#FAFBFC",
+        textTransform: 'capitalize',
+    },
+}
+
+export const fontNo19 = {
+    container: {
+        width: 78,
+        height: 24,
+    },
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 12,
+        lineHeight: 24,
+        color: "#FAFBFC",
+        textTransform: 'capitalize',
+    },
+}
+
+export const buttonNo1 = {
+    container: {
+        width: 80,
+        height: 32,
+        borderRadius: 6,
+        paddingTop: 4,
+        paddingLeft: 8,
+        paddingRight: 8,
+        paddingBottom: 4,
+        backgroundColor: "#82A5E3",
+        borderColor: "#82A5E3"
+    },
+}
+
+export const buttonNo2 = {
+    container: {
+        width: 100,
+        height: 32,
+        borderRadius: 6,
+        paddingTop: 4,
+        paddingLeft: 8,
+        paddingRight: 8,
+        paddingBottom: 4,
+        backgroundColor: "#054BC8",
+        borderColor: "#054BC8",
+    },
+}
+
+
+
+//------------------------------ CommentPage1 End------------------------------//
+
+
+
+//------------------------------ CommentPage2 End------------------------------//
+
+
+export const image1 = {
+    container: {
+        position: "absolute",
+        margin: 0,
+        padding: 0,
+    }
+}
+export const colorBorderCon11 = {
+    container: {
+        width: 25,
+        height: 100,
+        backgroundColor: "#F7F8FC",
+        position: "relative",
+        margin: 0,
+        padding: 0,
+    }
+}
+export const imageCon11 = {
+    container: {
+        position: "absolute",
+    }
+}
+export const imageCon111 = {
+    container: {
+        position: "absolute",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 30
+
+    }
+}
+
+export const fontNo20 = {
+    container: {
+        width: 156,
+        height: 26,
+    },
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 18,
+        lineHeight: 28,
+        color: "#333D66",
+    },
+}
+export const fontNo21 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 12,
+        lineHeight: 28,
+        color: "#333D66",
+    },
+}
+export const fontNo22 = {
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: 12,
+        lineHeight: 14,
+        color: "#9EA4B3",
+    }
+}
+export const plusBluebox = {
+    container: {
+        width: 28,
+        height: 29,
+        borderRadius: 6,
+        paddingTop: 4,
+        paddingLeft: 8,
+        paddingRight: 8,
+        paddingBottom: 4,
+        backgroundColor: "#0044BD"
+    },
+}
+
+
+
+
+export const fontNo23 = {
+    container: {
+        width: 106,
+        height: 24,
+    },
+    text: {
+        fontFamily: "Roboto",
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 12,
+        lineHeight: 24,
+        color: "#FAFBFC",
+        textTransform: 'capitalize',
+    },
+}
+
+export const documentBack = {
+    container: {
+        width: 350,
+        height: 400,
+        backgroundColor: "grey",
+    },
+}
+
+export const buttonNo3 = {
+    container: {
+        width: 130,
+        height: 32,
+        borderRadius: 6,
+        paddingTop: 4,
+        paddingLeft: 8,
+        paddingRight: 8,
+        paddingBottom: 4,
+        backgroundColor: "#054BC8",
+        borderColor: "#054BC8",
+    },
+}
+
+
+export const buttonNo4 = {
+    container: {
+        width: 70,
+        height: 32,
+        borderRadius: 6,
+        paddingTop: 4,
+        paddingLeft: 8,
+        paddingRight: 8,
+        paddingBottom: 4,
+        backgroundColor: "#054BC8",
+        borderColor: "#054BC8",
+    },
+}
+
+
+export const documentBack2 = {
+    container: {
+        width: 400,
+        height: 200,
+        backgroundColor: "FAFBFC",
+    },
+}
+
+export const width100 = {
+    container: {
+        width: "100%",
+    }
+}
+
+export const width500 = {
+    container: {
+        width: 250,
+    }
+}
+export const width300 = {
+    container: {
+        width: 400,
+    }
+}
+export const height100 = {
+    container: {
+        height: 480,
+    }
+}
+export const height700 = {
+    container: {
+        height: 650,
+    }
+}
+export const height20 = {
+    container: {
+        height: 100,
+    }
+}
+export const seemore = {
+    text: {
+        color: "#769fe8",
+        paddingLeft: 25,
+    }
+}
+export const imgBorder = {
+    container: {
+        borderRadius: 10,
+        height: 60,
+        width: 60,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    image: {
+        borderRadius: 30,
+    }
+}
+export const scrolloption = {
+    container: {
+        borderRadius: 10,
+        with: 400,
+        height: 500,
+        overflow: 'scroll',
+        border: 2,
+        borderColor: "#000",
+        overflowY: 'hidden',
+        overflowX: 'scroll'
+
+    },
+
+}
+
+export const tabHeight = {
+    container: {
+        height: 500,
+    }
+}
+
+export const profileimg = {
+    container: {
+        position: 'relative',
+        width: 150,
+        height: 150,
+        borderRadius: 150 / 2,
+        overflow: "hidden",
+        borderWidth: 3,
+        borderColor: "#0044BD",
+        alignItems: "center",
+        justifyContent: "center",
+    }
+}
+
+export const profileimg2 = {
+    container: {
+        position: 'relative',
+        width: 65,
+        height: 65,
+        borderRadius: 150 / 2,
+        overflow: "hidden",
+        alignItems: "center",
+        justifyContent: "center",
+        borderWidth: 2,
+        borderColor: "white",
+    }
+}
+
+export const profileimg3 = {
+    container: {
+        position: 'relative',
+        width: 40,
+        height: 40,
+        borderRadius: 150 / 2,
+        overflow: "hidden",
+        alignItems: "center",
+        justifyContent: "center",
+        borderWidth: 2,
+        borderColor: "white",
+    }
+}
